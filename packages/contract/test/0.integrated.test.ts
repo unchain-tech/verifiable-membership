3459cfeea0d36a97a8f6f2866dc5eb63978c85a2
SHØ <neila@users.noreply.github.com>
2023-01-10 17:10:51 +0900

    added solhint and test boilerplate

---
+++ packages/contract/test/0.integrated.test.ts
@@ -0,0 +1,1 @@
+require('./1.contractUpgrade.test.ts');
---
+++ packages/contract/test/1.contractUpgrade.test.ts
@@ -0,0 +1,8 @@
+// Load dependencies
+import { expect } from 'chai';
+import { Contract, ContractFactory } from 'ethers';
+
+let v1Contract: Contract;
+let v2Contract: Contract;
+
+describe('Contract Upgrade test', function () {});
