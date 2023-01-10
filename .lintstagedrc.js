3bdd11f6fdf6b7e320e55dffad502ab088d33c25
SHØ <neila@users.noreply.github.com>
2023-01-10 16:24:30 +0900

    added precommit hook

---
+++ .lintstagedrc.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const buildLintCommand = (filenames) =>
+  `yarn run rome check ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`;
+
+const buildFormatCommand = (filenames) =>
+  `yarn run rome format ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')} `;
+
+const buildSolhintCommand = (filenames) =>
+  `solhint --max-error 0 --ignore-path .gitignore ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')} `;
+
+module.exports = {
+  '**/*.{js,jsx,ts,tsx}': [buildLintCommand],
+  '**/*': [buildFormatCommand],
+  '**/*.sol': [buildSolhintCommand],
+};
