720e03522afd26e3411f607c126fe13a44cfc98b
junya-thinkactive-d <develop.junya@gmail.com>
2023-01-18 15:17:25 +0900

    change ts

---
+++ packages/vc/constants/index.ts
@@ -0,0 +1,32 @@
+import * as path from 'path';
+
+
+/**
+ * VC用サンプル画像フォルダ
+ */
+export const VC_IMAGE_PATH = path.join(__dirname, '../assets/vc-img/sample.svg');
+
+// 各種定数(ここは柔軟に入れ替えられるようにしてあげる必要あり)
+export const UUID_PREFIX = 'arn:uuid:';
+export const MEMBER_ID = '9415';
+export const WALLET_ADDRESS = '0x51908F598A5e0d8F1A3bAbFa6DF76F9704daD072';
+export const ISSUER_DID =
+  'https://blockcerts-20230113.storage.googleapis.com/profile.json';
+
+export const TITLE = 'UNCHAIN Membership VC';
+export const DESCRIPTION = 'これは、UNCHAINエントリー用のメンバーシップVCです。';
+export const DESCRIPTION_JP = 'This is a membership VC for UNCHAIN.';
+
+/**
+ * VCのテンプレ
+ */
+export const TEMPLATE_VC = {
+  '@context': [
+    'https://www.w3.org/2018/credentials/v1',
+    'https://w3id.org/blockcerts/v3',
+  ],
+  type: ['VerifiableCredential', 'BlockcertsCredential'],
+  credentialSubject: {
+    id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
+  },
+};
