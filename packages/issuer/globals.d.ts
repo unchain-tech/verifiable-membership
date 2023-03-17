053320d45e43ca04b29807d4bb25053ac998cb7a
nc163 <kou1992@gmail.com>
2023-03-17 15:48:49 +0000

    update:doc

--- packages/issuer/globals.d.ts
+++ packages/issuer/globals.d.ts
@@ -1,7 +1,5 @@
 declare namespace NodeJS {
-  // 環境変数名の定義
   interface ProcessEnv {
-    /** 現在の Node.js 実行環境 */
     readonly NODE_ENV: 'development' | 'production' | 'test';
 
     readonly ISSUER_ETH_ADDRESS_KEY: string;
