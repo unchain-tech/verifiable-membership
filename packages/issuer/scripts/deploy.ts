c8a3ae53d139ddd49b68a68cd10311858334bc0c
nc163 <kou1992@gmail.com>
2023-03-18 06:19:27 +0000

    refactor

--- packages/issuer/scripts/deploy.ts
+++ packages/issuer/scripts/deploy.ts
@@ -1,11 +1,6 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.utils.parseEther('0.001');
-
   const EthereumDIDRegistry = await ethers.getContractFactory(
     'EthereumDIDRegistry',
   );
@@ -13,11 +8,7 @@ async function main() {
 
   await contract.deployed();
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount,
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${contract.address}`,
-  );
+  console.log(`deployed to ${contract.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
